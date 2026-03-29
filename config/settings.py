e75d35c1dd50d42675be55ffab5dcc70d108507a
Jero <jero.infoservice@gmail.com>
2026-03-30 02:17:52 +0800

    Add bg image

--- config/settings.py
+++ config/settings.py
@@ -67,9 +67,9 @@ TIME_ZONE = 'Asia/Kuala_Lumpur'
 USE_I18N = True
 USE_TZ = True
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_DIRS = [BASE_DIR / 'static']
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-STATICFILES_DIRS = [BASE_DIR / 'Static']
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
